0f71d665738203a7956b238d1fc4682fba946687
Daniilsol200 <daniilsol200@gmail.com>
2025-03-10 18:25:25 +0300

    Начало работы

--- StreamDecoratorDemo2/StreamDecoratorDemo2/MainWindow.xaml.cs
+++ StreamDecoratorDemo2/StreamDecoratorDemo2/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Shapes;
 namespace StreamDecoratorDemo2
 {
     /// <summary>
-    /// Логика взаимодействия для MainWi
+    /// Логика взаимодействия для
     /// </summary>
     public partial class MainWindow : Window
     {
