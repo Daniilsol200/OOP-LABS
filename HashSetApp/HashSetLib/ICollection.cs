6192b3c7f5da0010628069dfaf303325cb5a57fc
Daniilsol200 <daniilsol200@gmail.com>
2025-03-17 17:56:57 +0300

    Добавление интерфейса

---
+++ HashSetApp/HashSetLib/ICollection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace HashSetLib
+{
+    public interface ICollection<T>
+    {
+        void Add(T item);
+        bool Remove(T item);
+        bool Contains(T item);
+        int Count { get; }
+        void Clear();
+        IEnumerator<T> GetEnumerator();
+    }
+}
